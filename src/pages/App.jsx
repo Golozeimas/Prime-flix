2b4090a216502bab30a1ce1978e31b238228787b
Matheus+ <joaosobramatheus@gmail.com>
2026-04-23 10:45:41 -0300

    initial commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,19 @@
+import "../style/main.css"
+
+
+function App(){
+    return(
+        <div> 
+            <header className="header">
+                <h1>
+                    Prime Flix
+                </h1>
+                <button>
+                    Meus filmes
+                </button>
+            </header>
+        </div>
+    )
+}
+
+export default App
