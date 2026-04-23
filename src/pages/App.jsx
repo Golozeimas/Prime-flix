fb7df8bf09080ae36af86beb77f5c2288da2af64
Matheus+ <joaosobramatheus@gmail.com>
2026-04-23 12:10:46 -0300

    feat: adiciona o uso de API para o site

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,26 +1,33 @@
 import { useEffect } from "react"
-import "../style/main.css"
 import { useState } from "react"
+import "../style/main.css"
 
 function App(){
     
-    const {filmes, setFilmes } = useState([])
-    filmes
-    setFilmes
-
+    const [filmes, setFilmes] = useState([])
+    
     useEffect(()=>{
-        fetch()
+        fetch("https://omdbapi.com/?s=batman&apikey=a17992ac&page=1")
+        .then((res)=> res.json())
+        .then((filme)=> setFilmes(filme.Search))
+        .catch((err)=> console.log(err))
     }, [])
+
+    if(filmes.length === 0){
+        return <h1> Carregando..</h1>
+    }
     
     return(
         <div> 
-            <main>
+            {filmes.map((filme)=>{
+            return <main key={filme.imdbID}>
                 <h2>
-                    Venom: Tempo de carnificina
+                    {filme.Title}
                 </h2>
-                <img src="public/PrimeFlix.png" alt="" />
+                <img src={filme.Poster} alt="Imagem do filme/série" />
                 <button>Acessar</button>
             </main>
+            })}
         </div>
     )
 }
